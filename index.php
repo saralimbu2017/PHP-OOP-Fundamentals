7c59d9d86c82545855c3623f11e08fdac5184602
saralimbu2017 <iamsebi7@gmail.com>
2022-02-08 18:38:01 +1100

    Adding comments

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 
 <?php
 class Person{
+  //attributes
   public $name;
 
+  //public methods
   public function getPersonName(){
     return "The name is:".$this->name;
   }
 }
+  //creating object
   $personOne = new Person;
   $personOne->name = "Sara";
   echo $personOne->getPersonName();
